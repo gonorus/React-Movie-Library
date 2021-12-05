9cf4b4388eea207de2ec0c5d5346002a59a14bd8
Dwi Anggono W.S.P <casperkepalabundar@gmail.com>
2021-12-05 10:28:21 +0700

    [DEV] added Movies Carousel component

---
+++ src/__tests__/components/MovieCarousel.test.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import MovieCarousel from '../../components/Base/MovieCarousel';
+
+test('Movie Thumbnail loaded', () => {
+  const movies = [
+    {
+      Id: '1', Title: 'Movie Title', ReleaseDate: new Date('2021-12-04'), ImagePath: 'http://localhost/example.jpg',
+    },
+    {
+      Id: '2', Title: 'Movie Title', ReleaseDate: new Date('2021-12-04'), ImagePath: 'http://localhost/example.jpg',
+    },
+    {
+      Id: '3', Title: 'Movie Title', ReleaseDate: new Date('2021-12-04'), ImagePath: 'http://localhost/example.jpg',
+    },
+    {
+      Id: '4', Title: 'Movie Title', ReleaseDate: new Date('2021-12-04'), ImagePath: 'http://localhost/example.jpg',
+    },
+    {
+      Id: '5', Title: 'Movie Title', ReleaseDate: new Date('2021-12-04'), ImagePath: 'http://localhost/example.jpg',
+    },
+    {
+      Id: '6', Title: 'Movie Title', ReleaseDate: new Date('2021-12-04'), ImagePath: 'http://localhost/example.jpg',
+    },
+    {
+      Id: '7', Title: 'Movie Title', ReleaseDate: new Date('2021-12-04'), ImagePath: 'http://localhost/example.jpg',
+    },
+    {
+      Id: '8', Title: 'Movie Title', ReleaseDate: new Date('2021-12-04'), ImagePath: 'http://localhost/example.jpg',
+    },
+    {
+      Id: '9', Title: 'Movie Title', ReleaseDate: new Date('2021-12-04'), ImagePath: 'http://localhost/example.jpg',
+    },
+    {
+      Id: '10', Title: 'Movie Title', ReleaseDate: new Date('2021-12-04'), ImagePath: 'http://localhost/example.jpg',
+    },
+  ];
+  const component = renderer.create(
+    <MovieCarousel movies={movies} />,
+  );
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
